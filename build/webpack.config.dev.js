4cf572b3372aa28190e798ae78032fda08db5932
Andrii Demianenko <andruxond98@gmail.com>
2018-12-31 06:58:20 +0200

    initial commit

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-style-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+}
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>{{ test }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      test: 'Hello World!'
+    }
+  }
+}
+</script>
+
