d9bd909e54a9769b6129a1e0f9db4d632e7255e3
Andrii Demianenko <andruxond98@gmail.com>
2019-01-13 14:53:58 +0200

    checked filters display

--- src/components/CheckedFilters.vue
+++ src/components/CheckedFilters.vue
@@ -1,15 +1,34 @@
 <template lang="html">
-  <div>
+  <section>
     <h4 class="title is-4">Checked Filters:</h4>
-    <span v-if="checkedFilters.length">{{ checkedFilters }}</span>
-    <span class="has-text-danger" v-else>No Filters Choosen!</span>
-  </div>
+    <div class="columns">
+      <div class="column">
+        <h6 class="title is-6">Checkbox Filters:</h6>
+        <div v-if="checkboxFilters.length">
+          <div class="content">
+            <ul>
+              <li v-for="filterVal in checkboxFilters"><span>{{ filterVal }}</span></li>
+            </ul>
+          </div>
+        </div>
+        <span class="has-text-danger" v-else>No Checkbox filters Choosen!</span>
+      </div>
+      <div class="column">
+        <h6 class="title is-6">Silder Filters:</h6>
+        <div v-if="sliderFilters.length">
+          <span>{{ sliderFilters }}</span>
+        </div>
+        <span class="has-text-danger" v-else>No Slider filters Choosen!</span>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   name: 'checked-filters',
   props: {
-    checkedFilters: Array
+    checkboxFilters: Array,
+    sliderFilters: Array
   },
   data () {
     return {}
